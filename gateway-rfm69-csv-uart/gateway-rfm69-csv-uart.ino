4fec2b344f3352c19a2a8bb02aa7eb9eb936b639
Andreas Motl <andreas@hiveeyes.org>
2016-09-30 11:38:20 +0200

    CSV-to-UART forwarder for real, prepare for Maker Faire Berlin 2016

--- gateway-rfm69-csv-uart/gateway-rfm69-csv-uart.ino
+++ gateway-rfm69-csv-uart/gateway-rfm69-csv-uart.ino
@@ -1,12 +1,11 @@
 /*
 
-                  Hiveeyes gateway-rfm69-beradio
+                  Hiveeyes gateway-rfm69-csv-uart
 
-   Receives sensor data over radio and forward it to the UART interface,
+   Receives CSV-based sensor data over radio and forwards it to the UART interface,
    which is connected to the serial port of the gateway machine.
 
-   Software release 0.5.1
-
+   Copyright (C) 2014-2016  Clemens Gruber
    Copyright (C) 2014-2016  Richard Pobering <einsiedlerkrebs@ginnungagap.org>
    Copyright (C) 2014-2016  Andreas Motl <andreas.motl@elmyra.de>
 
@@ -33,17 +32,9 @@
    Hiveeyes gateway sketch for Arduino based platforms
 
    This is a arduino sketch for the hiveeyes bee monitoring system.
-   The purpose is to receive data strings over a RFM69 radio module
-   and print them into serial out (RS-232), for further processing.
-
-   Since the data is are encapsulated in a BERadio character string
-   the next hop needs to read, unwrap it and send it to a data sink.
-   One option is BERadio pyhton module, which runs on arm's and send
-   the data further via MQTT as a json payload.
-
-   The creation of this code is strongly influenced by other projects, so
-   credits goes to
-   them: <https://hiveeyes.org/docs/beradio/README.html#credits>
+   The purpose is to receive data strings in CSV format over a RFM69
+   radio module and print them into serial out (RS-232), for further
+   processing.
 
    Feel free to adapt this code to your own needs.
 
@@ -61,48 +52,69 @@
 */
 
 
-int SENDERID2 = 2;
-//Date/Time,Weight,Outside Temp,Outside Humid,Inside Temp,Inside Humid,Brood Temp,Voltage
-String DATA2 = "4.143,28.9,33.7,25.2,34.7,26.6,26.8,26.6,26.6,26.4,4.26";
-int DATALEN2 = DATA2.length();
+#include <RFM69.h>                      // https://github.com/LowPowerLab/RFM69
+#include <SPI.h>
 
-//Date/Time,Weight,Outside Temp,Outside Humid,Inside Temp,Inside Humid,H1 Temp,H2 Temp,H3 Temp,H4 Temp,H5 Temp,Voltage
-int SENDERID3 = 3;
-String DATA3 = "1.001,16.5,83.9,16.2,81.6,16.1,3.9";
-int DATALEN3 = DATA3.length();
+#define NODEID      1
+#define NETWORKID   101
+#define FREQUENCY   RF69_868MHZ         //Match this with the version of your Moteino! (others: RF69_433MHZ, RF69_868MHZ)
+#define KEY         "demoKey4openHive"  //has to be same 16 characters/bytes on all nodes, not more not less!
+#define SERIAL_BAUD 115200
+
+#define SIMULATE
+
+
+RFM69 radio;
+bool promiscuousMode = false;           //set to 'true' to sniff all packets on the same network
 
 
 void setup() {
-  Serial.begin(115200);
+
+    #ifndef SIMULATE
+
+    // Setup serial interface
+    Serial.begin(SERIAL_BAUD);
+    delay(10);
+
+    // Setup radio module
+    radio.initialize(FREQUENCY, NODEID, NETWORKID);
+    radio.encrypt(KEY);
+    radio.promiscuous(promiscuousMode);
+
+    #endif
+
 }
+
+
+#ifdef SIMULATE
+
+// Weight,Outside Temp,Outside Humid,Inside Temp,Inside Humid,H1 Temp,H2 Temp,H3 Temp,H4 Temp,H5 Temp,Voltage
+int SENDERID2 = 2;
+String DATA2 = "4.143,28.9,33.7,25.2,34.7,26.6,26.8,26.6,26.6,26.4,4.26";
+
+// Weight,Outside Temp,Outside Humid,Inside Temp,Inside Humid,Brood Temp,Voltage
+int SENDERID3 = 3;
+String DATA3 = "1.001,16.5,83.9,16.2,81.6,16.1,3.9";
+
+#endif
 
 
 void loop() {
 
 
+#ifdef SIMULATE
 
-/*
-  if (radio.receiveDone())
-  {
-    Serial.println(radio.SENDERID, DEC)
-    
-    #theData.buff[i] = radio.DATA[i];
-    Serial.print((char)radio.DATA[i]);
-    Serial.println("\n")
-    }
+    /*
+    // Node #2
+    2016/05/09 15:07:22, 4.143, 28.9, 33.7, 25.2, 34.7, 26.6, 26.8, 26.6, 26.6, 26.4, 4.26
+    2016/05/09 15:27:22, 4.130, 26.1, 24.1, 25.2, 23.9, 27.1, 27.2, 27.2, 27.1, 27.0, 4.25
+    2016/05/09 15:47:22, 4.104, 28.2, 25.6, 28.1, 24.4, 25.7, 25.8, 25.6, 25.7, 25.6, 4.25
 
-*/
-/*
-//#2
-2016/05/09 15:07:22, 4.143, 28.9, 33.7, 25.2, 34.7, 26.6, 26.8, 26.6, 26.6, 26.4, 4.26
-2016/05/09 15:27:22, 4.130, 26.1, 24.1, 25.2, 23.9, 27.1, 27.2, 27.2, 27.1, 27.0, 4.25
-2016/05/09 15:47:22, 4.104, 28.2, 25.6, 28.1, 24.4, 25.7, 25.8, 25.6, 25.7, 25.6, 4.25 
-
-//#3
-2016/09/27 11:32:37,   1.001, 16.5, 83.9, 16.2, 81.6, 16.1, 3.94
-2016/09/27 11:42:37,   1.007, 17.0, 82.4, 16.8, 80.1, 16.6, 3.94
-2016/09/27 11:52:37,   1.006, 17.5, 79.2, 17.2, 76.6, 17.0, 3.94 
-*/
+    // Node #3
+    2016/09/27 11:32:37,   1.001, 16.5, 83.9, 16.2, 81.6, 16.1, 3.94
+    2016/09/27 11:42:37,   1.007, 17.0, 82.4, 16.8, 80.1, 16.6, 3.94
+    2016/09/27 11:52:37,   1.006, 17.5, 79.2, 17.2, 76.6, 17.0, 3.94
+    */
 
     Serial.print(SENDERID2, DEC);
     Serial.print(",,");
@@ -114,6 +126,23 @@ void loop() {
 
     delay(1000);
 
+#else
+
+    if (radio.receiveDone()) {
+
+        // Prefix CSV payload with NODE ID of sender node.
+        // Amendments for Make Faire 2016 to receive payloads from Open Hive hardware.
+        Serial.print(radio.SENDERID, DEC);
+        Serial.print(",");
+
+        // Forward the payload
+        for (byte i = 0; i < radio.DATALEN; i++) {
+            Serial.print((char)radio.DATA[i]);
+        }
+        Serial.print("\n");
+
+    }
+
+#endif
+
 }
-
-
