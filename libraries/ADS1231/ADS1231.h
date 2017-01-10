62487f14c35f5133406998a0626d65333493d183
Andreas Motl <andreas@hiveeyes.org>
2017-01-10 04:59:12 +0100

    Prepare changes to ADS1231: Fix newlines and whitespace

--- libraries/ADS1231/ADS1231.h
+++ libraries/ADS1231/ADS1231.h
@@ -1,6 +1,6 @@
     #ifndef ADS1231_H
     #define ADS1231_H
-     
+
 	#include <inttypes.h>
     #include <Arduino.h> //It is very important to remember this! note that if you are using Arduino 1.0 IDE, change "WProgram.h" to "Arduino.h"
 
@@ -15,9 +15,9 @@
 	  uint8_t dataPort        ;             // a pin number from 0 to 63
 	  uint8_t pwdnBit         ;             // a pin number from 0 to 63
 	  uint8_t pwdnPort        ;             // a pin number from 0 to 63
-	} ADS1231_t   ;  
+	} ADS1231_t   ;
 
-	
+
     class ADS1231 {
     public:
             ADS1231();
@@ -31,5 +31,5 @@
 	private:
 			uint8_t ADS1231Index;             // index into the channel data for this ADS1231
     };
-     
+
     #endif
