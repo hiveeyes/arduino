af57042cfc8936b608b887724d98b2d3a1320541
Andreas Motl <andreas@hiveeyes.org>
2017-04-24 22:20:14 +0200

    ADS1231 library: Fix compilation errors on Arduino Due (Atmel SAM/ARM)

--- libraries/ADS1231/ADS1231.h
+++ libraries/ADS1231/ADS1231.h
@@ -4,15 +4,26 @@
 	#include <inttypes.h>
     #include <Arduino.h> //It is very important to remember this! note that if you are using Arduino 1.0 IDE, change "WProgram.h" to "Arduino.h"
 
-    #ifdef __SAM3X8E__
+    // Atmel AVR
+    #if defined(__AVR__)
+      typedef volatile uint8_t PortReg;
+      typedef uint8_t PortMask;
+      typedef uint8_t PortSlot;
+
+    // Atmel SAM
+    #elif defined(__SAM3X8E__)
       typedef volatile RwReg PortReg;
       typedef uint32_t PortMask;
+      typedef Pio* PortSlot;
+
+    // Espressif ESP8266
     #elif defined(ESP8266)
       typedef volatile uint32_t PortReg;
       typedef uint32_t PortMask;
+      typedef uint8_t PortSlot;
+
     #else
-      typedef volatile uint8_t PortReg;
-      typedef uint8_t PortMask;
+      ERROR: Architecture/platform not supported.
     #endif
 
 	#define MAX_ADS1231   6
@@ -20,13 +31,13 @@
 	#define INVALID_ADS1231         255     // flag indicating an invalid servo index
 
 	typedef struct  {
-	  uint8_t sclBit          ;             // a pin number from 0 to 63
-	  uint8_t sclPort         ;             // a pin number from 0 to 63
-	  uint8_t dataBit         ;             // a pin number from 0 to 63
-	  uint8_t dataPort        ;             // a pin number from 0 to 63
-	  uint8_t pwdnBit         ;             // a pin number from 0 to 63
-	  uint8_t pwdnPort        ;             // a pin number from 0 to 63
-	} ADS1231_t   ;
+	  uint8_t  sclBit         ;             // a pin number from 0 to 63
+	  PortSlot sclPort        ;             // a pin number from 0 to 63
+	  uint8_t  dataBit        ;             // a pin number from 0 to 63
+	  PortSlot dataPort       ;             // a pin number from 0 to 63
+	  uint8_t  pwdnBit        ;             // a pin number from 0 to 63
+	  PortSlot pwdnPort       ;             // a pin number from 0 to 63
+	} ADS1231_t;
 
 
     class ADS1231 {
--- libraries/ADS1231/README.rst
+++ libraries/ADS1231/README.rst
@@ -5,7 +5,30 @@ ADS1231 library
 :author: aerowind
 :date: Jan 31, 2014
 
-From `ADS1231_beta.zip`_ via `Topic "SPI Load cell chip ADS1231"`_.
+************
+Introduction
+************
+Arduino-compatible driver library for ADS1231_ Low-Noise, 24-Bit Analog-to-Digital Converter from Texas Instruments.
+Original source code taken from `ADS1231_beta.zip`_ via Arduino forum topic `SPI Load cell chip ADS1231`_.
 
+
+*********
+Changelog
+*********
+
+2017-04-24
+==========
+- Fix compilation errors on Arduino Due (Atmel SAM/ARM)
+
+2017-01-10
+==========
+- Fix compilation errors on ESP8266
+
+2016-07-07
+==========
+- Initial import
+
+
+.. _ADS1231: http://www.ti.com/product/ADS1231
 .. _ADS1231_beta.zip: http://forum.arduino.cc/index.php?action=dlattach;topic=131086.0;attach=67564
-.. _Topic "SPI Load cell chip ADS1231": http://forum.arduino.cc/index.php?topic=131086.msg1570340#msg1570340
+.. _SPI Load cell chip ADS1231: http://forum.arduino.cc/index.php?topic=131086.msg1570340#msg1570340
