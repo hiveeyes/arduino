114bf93d955fd925c4c42783617fbcb37580011c
Andreas Motl <andreas@hiveeyes.org>
2023-06-03 14:07:23 +0200

    [TerkinTelemetry] Add transmitter component for CrateDB

---
+++ libraries/TerkinTelemetry/examples/http-cratedb/config.h
@@ -0,0 +1,42 @@
+// ******************************************
+//             User configuration
+// ******************************************
+
+// Connectivity
+// ============
+
+// Network
+// -------
+// TODO: For example, provide configuration settings for Wi-Fi connectivity.
+
+// Telemetry
+// ---------
+#define CRATEDB_URL         "http://localhost:4200/"
+#define CRATEDB_USERNAME    "crate"
+#define CRATEDB_PASSWORD    ""
+
+#define CHANNEL_REALM       "mqttkit-1"
+#define CHANNEL_OWNER       "testdrive"
+#define CHANNEL_SITE        "area-42"
+#define CHANNEL_DEVICE      "pipa-telemetry-http-cratedb"
+
+// Sensors
+// =======
+
+// TODO: No sensors yet. This is just a telemetry demo program.
+
+
+// ******************************************
+//           Developer configuration
+// ******************************************
+
+// Debugging
+// ---------
+/*
+#define DEBUG
+#define DEBUG_RADIO
+#define DEBUG_FLASH
+#define DEBUG_SENSORS
+#define DEBUG_SERIAL
+#define DEBUG_ENCODE
+*/
